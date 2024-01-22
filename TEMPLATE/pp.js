38d966c259277e498be1746152894cf1ae6c0ab6
ZAIN SHEIKH <105550667+Zainshk64@users.noreply.github.com>
2024-01-22 17:27:19 +0500

    Add files via upload

---
+++ TEMPLATE/pp.js
@@ -0,0 +1,35 @@
+// const but = document.getElementById("light").addEventListener(
+//   "click",
+//   function () {
+//     document.body.style.background = "black";
+//         document.body.style.color="white";
+
+//   }
+//   .addEventListener('mouseover', function(){
+//     document.body.style.background = "none";
+//     document.body.style.color="none";
+//   })
+// );
+
+// const butt = document.getElementById("light");
+
+function light(){
+    document.body.style.background="rgba(78, 135, 199, 0.583)";
+    document.body.style.color="white";
+    const butt = document.getElementById("light").style.color="white";
+    const but = document.getElementById("light").style.background="black";
+
+
+
+ 
+
+}
+function light1(){
+    document.body.style.color="black";
+    document.body.style.background="none";
+    const butt = document.getElementById("light").style.color="black";
+    const but = document.getElementById("light").style.background="white";
+
+
+
+}
